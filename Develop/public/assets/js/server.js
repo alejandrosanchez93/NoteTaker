96b1d3bd01e7b15fa59a928da1c575d43c5611eb
Alejandro Sanchez <alejandrosanchezwe@outlook.com>
2020-10-23 00:25:42 -0500

    added routes and read/write file function

---
+++ Develop/public/assets/js/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log("App listening on PORT: " + PORT));
+
+app.use(express.static("/public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../index.html"));
+})
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../notes.html"));
+})
+
+app.get("/api/notes", (req, res) => {
+    const noteList = JSON.parse(fs.readFileSync("../../../db/db.json", "utf8"));
+
+    res.json(noteList);
+})
+
+app.post("/api/notes", (req, res) => {
+    const newNote = req.body;
+
+    newNote.id = Math.floor(Math.random() * 100);
+    console.log("success a note has been entered: " + JSON.stringify(newNote));
+
+
+    const noteArray = JSON.parse(fs.readFileSync("../../../db/db.json", "utf8"));
+    noteArray.push(newNote);
+
+    fs.writeFileSync("../../../db/db.json", JSON.stringify(noteArray), "utf8");
+    res.json(noteArray);
+})
+
+
